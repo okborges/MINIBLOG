aaf80465ce0a55d8eb04d0a4dd740c58f27ca439
okborges <rafael.pb@partner.samsung.com>
2023-10-24 13:53:24 -0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/authContext";
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 
+// hooks
 import { useState, useEffect } from "react";
 import { useAuthentication } from "./hooks/useAuthentication";
 
+// pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Post from "./pages/Post/Post";
+
+// components
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Search from "./pages/Search/Search";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
-import Search from "./pages/Search/Search";
+import EditPost from "./pages/EditPost/EditPost";
 
-import "./App.css";
-import Post from "./pages/Post/Post";
+// context
+import { AuthProvider } from "./context/authContext";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -31,11 +38,11 @@ function App() {
   }, [auth]);
 
   if (loadingUser) {
-    return <p>carregando...</p>;
+    return <p>Carregando...</p>;
   }
 
   return (
-    <>
+    <div className="App">
       <AuthProvider value={{ user }}>
         <BrowserRouter>
           <Navbar />
@@ -43,8 +50,16 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
-              <Route path="/search" element={<Search />} />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/posts/edit/:id"
+                element={user ? <EditPost /> : <Navigate to="/login" />}
+              />
               <Route path="/posts/:id" element={<Post />} />
+              <Route path="/search" element={<Search />} />
               <Route
                 path="/login"
                 element={!user ? <Login /> : <Navigate to="/" />}
@@ -52,10 +67,6 @@ function App() {
               <Route
                 path="/register"
                 element={!user ? <Register /> : <Navigate to="/" />}
-              />
-              <Route
-                path="/posts/create"
-                element={user ? <CreatePost /> : <Navigate to="/login" />}
               />
               <Route
                 path="/dashboard"
@@ -66,7 +77,7 @@ function App() {
           <Footer />
         </BrowserRouter>
       </AuthProvider>
-    </>
+    </div>
   );
 }
 
